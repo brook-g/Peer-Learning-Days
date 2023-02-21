3928c92870f197894d0537bfe941e0b17674814b
Brook <broook@skiff.com>
2023-02-21 05:36:20 -0800

    first repo commit

---
+++ README.md
@@ -0,0 +1,1 @@
+The repository is a collection of small practice projects, and practice exercise from Peer Learning Days.
---
+++ scripts/ASCII_finder.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    char x;
+    printf("Enter any character: ");
+    scanf("%c", &x);
+    
+    printf("The ASCII value of %c is: %d");
+    return (0);
+}
